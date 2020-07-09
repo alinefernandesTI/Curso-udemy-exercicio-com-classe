3de9a88e9fbd8a455bf782439017598a3cfed5ec
alinefernandesTI <56439822+alinefernandesTI@users.noreply.github.com>
2020-07-09 13:26:44 -0300

    Create atividadeClasseAluno.java

---
+++ atividadeClasseAluno.java
@@ -0,0 +1,18 @@
+
+package atividade;
+
+
+public class aluno {
+    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+ 
+   
+}
