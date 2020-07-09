d76d55c28bdc837cca11336da54612e068bc0c96
alinefernandesTI <56439822+alinefernandesTI@users.noreply.github.com>
2020-07-09 13:29:52 -0300

    Create Exercicio5ClassePeca.java

---
+++ Exercicio5ClassePeca.java
@@ -0,0 +1,51 @@
+package exercicio5;
+
+public class peca {
+
+
+private int codigo;
+    private int numero;
+    private float valor;    
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+}
